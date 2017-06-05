79ebda278926148f583cfd4cb5b00938c026d098
drummer0417 <hans@androidappfactory.nl>
2017-06-05 23:19:10 +0200

    Moved style from nav.jsx to app.scss

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -9,8 +9,8 @@ var Nav = React.createClass({
         <div className="top-bar-left menu-text-color">
           <ul className="menu">
             <li className="menu-text">Timer App</li>
-            <li><IndexLink to='/' activeClassName='active' activeStyle={{fontWeight: 'bold', color: 'blue'}}>Timer</IndexLink></li>
-            <li><Link to='/countdown' activeClassName='active' activeStyle={{fontWeight: 'bold', color: 'blue'}}>Countdown</Link></li>
+            <li><IndexLink to='/' activeClassName='active-link'>Timer</IndexLink></li>
+            <li><Link to='/countdown' activeClassName='active-link'>Countdown</Link></li>
           </ul>
         </div>
         <div className="top-bar-right menu-text-color">
