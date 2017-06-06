ef6c222b05674c6dd325a30dca224604b7a86c8c
drummer0417 <hans@androidappfactory.nl>
2017-06-06 20:48:30 +0200

    Added style to top-bar 2

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,26 @@
+var React = require('react');
+var { Link, IndexLink } = require('react-router');
+
+var Navigation = React.createClass({
+
+  render: function() {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">Timer App</li>
+            <li><IndexLink to='/' activeClassName='active-link'>Timer</IndexLink></li>
+            <li><Link to='/countdown' activeClassName='active-link'>Countdown</Link></li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li className="menu-text">by: AndroidAppFactory.nl</li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+})
+
+module.exports = Navigation;
