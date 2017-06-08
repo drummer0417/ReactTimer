6985d50d4bd0ebc2a8e827d6ad6db44d1299168b
drummer0417 <hans@androidappfactory.nl>
2017-06-08 21:29:21 +0200

    Setup countdown component

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,11 +1,11 @@
-var React = require('react');
+ var React = require('react');
 var Clock = require('Clock');
 var Countdown = React.createClass({
   render: function() {
     return(
       <div>
         <h1 className="text-center page-title">Countdown</h1>
-        <Clock totalSeconds={90} />
+        <Clock totalSeconds={128} />
       </div>
     )
   }
